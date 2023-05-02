6fd9cbb73b5e6bd8c06a931984291018be1f1dda
Alessandro Bettini <abettini@c1r1p3.42firenze.it>
2023-05-02 10:58:39 +0200

    fede non scassare la mici, viva lapa pera nel lago duria

--- philo/philo_death.c
+++ philo/philo_death.c
@@ -6,7 +6,7 @@
 /*   By: abettini <abettini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 14:53:55 by abettini          #+#    #+#             */
-/*   Updated: 2023/04/28 15:40:17 by abettini         ###   ########.fr       */
+/*   Updated: 2023/05/02 10:51:35 by abettini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,21 @@ int	ft_calc_death(t_philo *philo)
 	return (check);
 }
 
+int	ft_check_full(t_philo *philos)
+{
+	int			check;
+	static int	count;
+
+	check = 0;
+	pthread_mutex_lock(&philos->info->meal);
+	while (philos[count].full)
+		count++;
+	if (count == philos->info->n_of_philos)
+		check = 1;
+	pthread_mutex_unlock(&philos->info->meal);
+	return (check);
+}
+
 void	*ft_philos_death(void *arg)
 {
 	t_philo	*philos;
@@ -36,7 +51,7 @@ void	*ft_philos_death(void *arg)
 
 	philos = (t_philo *)arg;
 	i = 0;
-	while (1)
+	while (!ft_check_full(philos))
 	{
 		if (ft_calc_death(&philos[i]))
 		{
@@ -45,7 +60,7 @@ void	*ft_philos_death(void *arg)
 				philos[i].id, "died");
 			philos->info->deaths++;
 			pthread_mutex_unlock(&philos->info->print);
-			return (NULL);
+			break ;
 		}
 		i++;
 		if (i == philos->info->n_of_philos)
